95eed95cf5f0ffb10cf7b2b2acad249363c3c10d
dxj <362775721>
2022-03-18 00:35:08 +0800

    增加kratos模版

---
+++ server/helloworld/internal/biz/greeter.go
@@ -0,0 +1,39 @@
+package biz
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/errors"
+	"github.com/go-kratos/kratos/v2/log"
+	v1 "helloworld/api/helloworld/v1"
+)
+
+var (
+	ErrUserNotFound = errors.NotFound(v1.ErrorReason_USER_NOT_FOUND.String(), "user not found")
+)
+
+type Greeter struct {
+	Hello string
+}
+
+type GreeterRepo interface {
+	CreateGreeter(context.Context, *Greeter) error
+	UpdateGreeter(context.Context, *Greeter) error
+}
+
+type GreeterUsecase struct {
+	repo GreeterRepo
+	log  *log.Helper
+}
+
+func NewGreeterUsecase(repo GreeterRepo, logger log.Logger) *GreeterUsecase {
+	return &GreeterUsecase{repo: repo, log: log.NewHelper(logger)}
+}
+
+func (uc *GreeterUsecase) Create(ctx context.Context, g *Greeter) error {
+	return uc.repo.CreateGreeter(ctx, g)
+}
+
+func (uc *GreeterUsecase) Update(ctx context.Context, g *Greeter) error {
+	return uc.repo.UpdateGreeter(ctx, g)
+}
